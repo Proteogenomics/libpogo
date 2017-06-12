ef58a8672bd1d0bf33471bbc3eca1d718de94782
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-06-12 16:03:05 +0100

    PoGo file header serializer

---
+++ src/main/java/uk/ac/ebi/pride/proteogenomics/pogo/model/export/file/PoGoHeaderSerializer.java
@@ -0,0 +1,20 @@
+package uk.ac.ebi.pride.proteogenomics.pogo.model.export.file;
+
+import java.util.List;
+
+/**
+ * Project: libpogo
+ * Package: uk.ac.ebi.pride.proteogenomics.pogo.model.export.file
+ * Timestamp: 2017-06-12 15:58
+ * ---
+ * © 2017 Manuel Bernal Llinares <mbdebian@gmail.com>
+ * All rights reserved.
+ */
+
+public abstract class PoGoHeaderSerializer {
+    public abstract String getDelimiter();
+
+    public String serializeHeader(List<String> headers) {
+        return String.join(getDelimiter(), headers);
+    }
+}
