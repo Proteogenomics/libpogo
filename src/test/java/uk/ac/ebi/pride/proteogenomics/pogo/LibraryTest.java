c8003ee167353e704f83092fa767c40c238fc6e1
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-06-12 16:56:11 +0100

    unit test placeholder for the library

---
+++ src/test/java/uk/ac/ebi/pride/proteogenomics/pogo/LibraryTest.java
@@ -0,0 +1,19 @@
+package uk.ac.ebi.pride.proteogenomics.pogo;
+
+import uk.ac.ebi.pride.proteogenomics.pogo.model.PoGoEntry;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Project: libpogo
+ * Package: uk.ac.ebi.pride.proteogenomics.pogo
+ * Timestamp: 2017-06-12 16:51
+ * ---
+ * © 2017 Manuel Bernal Llinares <mbdebian@gmail.com>
+ * All rights reserved.
+ */
+
+public class LibraryTest {
+
+}
