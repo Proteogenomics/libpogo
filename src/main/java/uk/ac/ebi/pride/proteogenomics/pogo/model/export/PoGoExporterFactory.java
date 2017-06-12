5f6749ae1341a245b6c486a86ffdf805b054200e
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-06-12 15:42:37 +0100

    PoGo exporter factory - tabular file exporter

---
+++ src/main/java/uk/ac/ebi/pride/proteogenomics/pogo/model/export/PoGoExporterFactory.java
@@ -0,0 +1,16 @@
+package uk.ac.ebi.pride.proteogenomics.pogo.model.export;
+
+/**
+ * Project: libpogo
+ * Package: uk.ac.ebi.pride.proteogenomics.pogo.model.export
+ * Timestamp: 2017-06-12 15:34
+ * ---
+ * © 2017 Manuel Bernal Llinares <mbdebian@gmail.com>
+ * All rights reserved.
+ */
+
+public class PoGoExporterFactory {
+    public static PoGoExporter getTabularFileExporter(String fileName) {
+        return new PoGoFileExporter(fileName, new PoGoEntrySerializerTabDelimiter());
+    }
+}
