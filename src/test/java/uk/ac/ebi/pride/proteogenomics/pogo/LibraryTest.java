538ac4e95a67ed6dfbc1c6d32e91693e2f5650a7
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-06-13 10:55:03 +0100

    library test, import / export PoGo data

--- src/test/java/uk/ac/ebi/pride/proteogenomics/pogo/LibraryTest.java
+++ src/test/java/uk/ac/ebi/pride/proteogenomics/pogo/LibraryTest.java
@@ -1,8 +1,23 @@
 package uk.ac.ebi.pride.proteogenomics.pogo;
 
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import uk.ac.ebi.pride.proteogenomics.pogo.model.PoGoEntry;
+import uk.ac.ebi.pride.proteogenomics.pogo.model.export.PoGoExporterFactory;
+import uk.ac.ebi.pride.proteogenomics.pogo.model.reader.PoGoFileReader;
+import uk.ac.ebi.pride.proteogenomics.pogo.model.reader.PoGoReaderFactory;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileVisitOption;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 /**
@@ -15,5 +30,43 @@ import java.util.List;
  */
 
 public class LibraryTest {
+    private static final Logger logger = LoggerFactory.getLogger(LibraryTest.class);
+    private String tmpFolderName = "tmp";
+    private File tmpFolder = null;
 
+    private List<PoGoEntry> getTestSmallDataset() {
+        ClassLoader classLoader = getClass().getClassLoader();
+        File file = new File(classLoader.getResource("Testfile_small.txt").getFile());
+        return PoGoReaderFactory.getTabularPoGoFileReader(file.getAbsolutePath()).readAllPoGoEntries();
+    }
+
+    @Before
+    public void prepareTestsOutputFolder() {
+        tmpFolder = new File(tmpFolderName);
+        if (!tmpFolder.exists()) {
+            tmpFolder.mkdirs();
+        }
+    }
+
+    public void cleanTestsOutputFolder() {
+        File tmpFolder = new File(tmpFolderName);
+        Path tmpFolderPath = Paths.get(tmpFolder.getAbsolutePath());
+        try {
+            Files.walk(tmpFolderPath, FileVisitOption.FOLLOW_LINKS)
+                    .sorted(Comparator.reverseOrder())
+                    .map(Path::toFile)
+                    .peek(System.out::println)
+                    .forEach(File::delete);
+        } catch (IOException e) {
+            logger.error("AN ERROR OCCURRED WHILE CLEANING TMP FOLDER FOR TESTS AT '{}', due to '{}'",
+                    tmpFolderPath.toAbsolutePath().toString(),
+                    e.getMessage());
+        }
+    }
+
+    @Test
+    public void tabFileExport() {
+        Path filePath = Paths.get(tmpFolder.getAbsolutePath(), "exported_dataset.pogo");
+        PoGoExporterFactory.getTabularFileExporter(filePath.toAbsolutePath().toString()).export(getTestSmallDataset());
+    }
 }
